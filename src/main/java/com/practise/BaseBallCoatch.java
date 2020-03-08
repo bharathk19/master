f8f38978cc55d02f78b001db6817df3f92991e11
bharathk19 <kumar.bharath289@gmail.com>
2020-03-08 15:45:46 -0400

    Commiting all the files

--- src/main/java/com/practise/BaseBallCoatch.java
+++ src/main/java/com/practise/BaseBallCoatch.java
@@ -2,11 +2,17 @@ package com.practise;
 
 public class BaseBallCoatch implements Coatch{
 
+    private  FortuneService fortuneService;
+
+    public BaseBallCoatch(FortuneService thefortuneService)
+    {
+        fortuneService=thefortuneService;
+    }
     public String DailyWorkout() {
         return "Spend 30 min on bowling practise";
     }
 
     public String DailyFortune() {
-        return null;
+        return fortuneService.getFortune();
     }
 }
